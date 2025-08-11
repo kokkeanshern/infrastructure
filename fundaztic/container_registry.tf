dd5f1671a85318aaf3e5ad5810b28c4b775384c7
kokkeanshern <kokkeanshern@gmail.com>
2025-08-11 23:31:41 +0800

    remove resource

--- fundaztic/container_registry.tf
+++ fundaztic/container_registry.tf
@@ -1,6 +1,6 @@
 resource "oci_artifacts_container_repository" "fundaztic" {
   compartment_id = var.compartment_id
-  display_name   = "fundaztic/"
+  display_name   = "fundaztic"
 
   is_immutable = false
   is_public    = false
