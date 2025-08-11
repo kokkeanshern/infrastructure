c69e77906a728f3439e61a9778914b4ed2247c54
kokkeanshern <kokkeanshern@gmail.com>
2025-08-11 23:12:43 +0800

    fix state backend

--- fundaztic/main.tf
+++ fundaztic/main.tf
@@ -3,9 +3,18 @@ provider "oci" {
 
 terraform {
   backend "oci" {
-    namespace = "ax1h9bph8nyo"
-    bucket    = "terraform-state-backend"
-    key       = "fundaztic/dev.tfstate"
-    region    = "ap-singapore-1"
+    namespace                   = "ax1h9bph8nyo"
+    bucket                      = "terraform-state-backend"
+    key                         = "fundaztic/dev.tfstate"
+    region                      = "ap-singapore-1"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    use_path_style              = true
+    skip_s3_checksum            = true
+    skip_metadata_api_check     = true
+    endpoints = {
+      s3 = "https://ax1h9bph8nyo.compat.objectstorage.ap-singapore-1.oraclecloud.com"
+    }
   }
 }
