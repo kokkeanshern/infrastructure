6cefd2d4b2fa6af3a143b40f43deaf365e39034d
kokkeanshern <69450995+kokkeanshern@users.noreply.github.com>
2025-08-11 18:00:22 +0800

    add new repository (#3)
    auto-merge

---
+++ fundaztic/container_registry.tf
@@ -0,0 +1,8 @@
+resource "oci_artifacts_container_repository" "fundaztic" {
+  compartment_id = var.compartment_id
+  display_name   = "fundaztic/"
+
+  is_immutable = false
+  is_public    = false
+
+}
