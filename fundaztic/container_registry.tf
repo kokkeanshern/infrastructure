ea5cb7af0f54e04ede29280e39f7c1fa731ec120
kokkeanshern <69450995+kokkeanshern@users.noreply.github.com>
2025-08-12 12:12:49 +0800

    destroy old repo and create new dev and prod ones (#9)
    auto-merge

--- fundaztic/container_registry.tf
+++ fundaztic/container_registry.tf
@@ -1,6 +1,15 @@
-resource "oci_artifacts_container_repository" "fundaztic" {
+resource "oci_artifacts_container_repository" "fundaztic-dev" {
   compartment_id = var.compartment_id
-  display_name   = "fundaztic"
+  display_name   = "fundaztic/dev"
+
+  is_immutable = false
+  is_public    = false
+
+}
+
+resource "oci_artifacts_container_repository" "fundaztic-prod" {
+  compartment_id = var.compartment_id
+  display_name   = "fundaztic/prod"
 
   is_immutable = false
   is_public    = false
