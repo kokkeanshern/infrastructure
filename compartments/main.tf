c7c7ee2ef579ec65d8549d9d43c7a53bc0f4feaf
kokkeanshern <kokkeanshern@gmail.com>
2025-08-12 23:16:34 +0800

    tmp

---
+++ compartments/main.tf
@@ -0,0 +1,11 @@
+# provider "oci" {
+# }
+
+# terraform {
+#   backend "oci" {
+#     namespace = "ax1h9bph8nyo"
+#     bucket    = "terraform-state-backend"
+#     key       = "fundaztic/terraform.tfstate"
+#     region    = "ap-singapore-1"
+#   }
+# }
