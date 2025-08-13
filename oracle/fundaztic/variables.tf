b5e13ccdc1d90e102e7b6101d917119a026b7458
kokkeanshern <kokkeanshern@gmail.com>
2025-08-13 13:54:35 +0800

    create repos

---
+++ oracle/fundaztic/variables.tf
@@ -0,0 +1,5 @@
+variable "compartment_id" {
+  description = "The OCID of the compartment where resources will be created."
+  type        = string
+  default     = "ocid1.compartment.oc1..aaaaaaaawutq5bmyehb2obqa3hcvstm5gzmboh62p7ixwwhdvgz2r73kvynq"
+}
