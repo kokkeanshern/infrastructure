c5166b6eaba62f0859552397086698470cc7bfdb
kokkeanshern <kokkeanshern@gmail.com>
2025-08-13 13:38:43 +0800

    fix bucket name

--- oracle/network/main.tf
+++ oracle/network/main.tf
@@ -4,7 +4,7 @@ provider "oci" {
 terraform {
   backend "oci" {
     namespace = "ax1h9bph8nyo"
-    bucket    = "network"
+    bucket    = "bucket-tfstate-network"
     key       = "oracle/network/terraform.tfstate"
     region    = "ap-singapore-1"
   }
