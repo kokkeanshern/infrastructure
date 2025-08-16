4eec4c4d42e5cbff36e2e66e3b2f7804c0d6b431
kokkeanshern <kokkeanshern@gmail.com>
2025-08-16 23:26:26 +0800

    fix compartment id for netwrok resources

--- oracle/fundaztic/data.tf
+++ oracle/fundaztic/data.tf
@@ -1,14 +1,14 @@
 data "oci_identity_availability_domains" "ads" {
-  compartment_id = var.compartment_id
+  compartment_id = var.compartment_id_network
 }
 
 data "oci_core_vcns" "vcn_shared" {
-  compartment_id = var.compartment_id
+  compartment_id = var.compartment_id_network
   display_name   = "vcn_shared"
 }
 
 data "oci_core_subnets" "subnet_shared_private" {
-  compartment_id = var.compartment_id
+  compartment_id = var.compartment_id_network
   display_name   = "subnet-shared-private"
   vcn_id         = data.oci_core_vcns.vcn_shared.virtual_networks[0].id
 }
