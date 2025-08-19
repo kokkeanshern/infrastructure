2655d24c4c24c13f50e459dbe3986c3bb4a547d4
kokkeanshern <kokkeanshern@gmail.com>
2025-08-19 21:33:48 +0800

    new syntax

--- oracle/fundaztic/dynamic_group.tf
+++ oracle/fundaztic/dynamic_group.tf
@@ -1,17 +1,15 @@
 resource "oci_identity_dynamic_group" "instance_dynamic_group" {
   compartment_id = var.tenancy_ocid
   description    = "Dynamic group for Fundaztic instance management"
-  # matching_rule  = "ALL instance.compartment.id = ${var.compartment_id}"
-  matching_rule = "ALL instance.compartment.id = ocid1.compartment.oc1..aaaaaaaawutq5bmyehb2obqa3hcvstm5gzmboh62p7ixwwhdvgz2r73kvynq"
-  name          = "fundaztic-instance-dynamic-group"
+  matching_rule  = "ALL {instance.compartment.id = '${var.compartment_id}'}"
+  name           = "fundaztic-instance-dynamic-group"
 }
 
 resource "oci_identity_policy" "fundaztic_ocir_policy" {
   name           = "fundaztic-ocir-policy"
   description    = "Allow Fundaztic instance dynamic group to pull images from OCIR"
   compartment_id = var.tenancy_ocid
-
   statements = [
-    "Allow dynamic-group fundaztic-instance-dynamic-group to read repos in compartment '${var.compartment_id}'",
+    "Allow dynamic-group fundaztic-instance-dynamic-group to read repos in compartment ${var.compartment_id}",
   ]
 }
